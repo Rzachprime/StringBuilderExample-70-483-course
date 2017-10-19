7bd57d1218eb228378b0ce49b2bb1cc20710574c
Richard Z Troester <rzachprime@gmail.com>
2017-10-19 03:16:57 -0400

    added several examples of stringbuilder

--- StringBuilderExample/Program.cs
+++ StringBuilderExample/Program.cs
@@ -10,6 +10,20 @@ namespace StringBuilderExample
     {
         static void Main(string[] args)
         {
+            string s1 = "This is an example of";
+            s1 = s1 + " string concatenation...";
+            Console.WriteLine(s1);
+
+            StringBuilder sb2 = new StringBuilder("This is an example of");
+            sb2.Append(" string concatenation...");
+            Console.WriteLine(sb2);
+
+            sb2.AppendLine();
+            sb2.Append(" using Stringbuilder");
+            Console.WriteLine(sb2);
+
+            sb2.Replace("Stringbuilder", "The Stringbuilder Class...");
+            Console.WriteLine(sb2);
         }
     }
 }
